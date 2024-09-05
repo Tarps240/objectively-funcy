7e52c1d1452b5a793d6f6fc4b12251c83e46ec5c
Tarps240 <sl3966@gmail.com>
2024-09-05 05:08:42 -0700

    Finish Homework

---
+++ main.js
@@ -0,0 +1,93 @@
+// YOUR CODE GOES HERE!!!
+/*************************/
+
+function getFirstName(person) {
+  return person.firstName;
+}
+
+function getLastName(person) {
+  return person.lastName;
+}
+
+function getFullName(person) {
+  return person.firstName + ' ' + person.lastName;
+}
+
+function setFirstName(person, newFirstName) {
+  person.firstName = newFirstName;
+}
+
+function setAge(person, newAge) {
+  person.age = newAge;
+}
+
+function giveBirthday(person) {
+  
+  if (person.hasOwnProperty('age')) {
+    person.age += 1;
+  } else {
+    
+    person.age = 1;
+  }
+}
+
+const marry = (person1, person2) => {
+  person1.married = true
+  person2.married = true
+  person1.spouseName = getFullName(person2)
+  person2.spouseName = getFullName(person1)
+}
+
+const divorce = (person1, person2) => {
+  person1.married = false;
+  person2.married = false;
+  delete person1.spouseName;
+  delete person2.spouseName;
+}
+
+/****************************/
+// Our code here. Don't touch!
+if (typeof getFirstName === 'undefined') {
+  getFirstName = undefined
+}
+
+if (typeof getLastName === 'undefined') {
+  getLastName = undefined
+}
+
+if (typeof getFullName === 'undefined') {
+  getFullName = undefined
+}
+
+if (typeof setFirstName === 'undefined') {
+  setFirstName = undefined
+}
+
+if (typeof setAge === 'undefined') {
+  setAge = undefined
+}
+
+if (typeof giveBirthday === 'undefined') {
+  giveBirthday = undefined
+}
+
+if (typeof marry === 'undefined') {
+  marry = undefined
+}
+
+if (typeof divorce === 'undefined') {
+  divorce = undefined
+}
+
+
+
+module.exports = {
+  getFirstName,
+  getLastName,
+  getFullName,
+  setFirstName,
+  setAge,
+  giveBirthday,
+  marry,
+  divorce,
+}
